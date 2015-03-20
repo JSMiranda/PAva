132442df62a58362e7750884676c8bbdb14bb220
Joao Diogo Figueira Pereira <ist168509@linbase.rnl.tecnico.ulisboa.pt>
2015-03-20 16:54:45 +0000

    proj inicial

--- lab02/src/lab02/Main.java
+++ lab02/src/lab02/Main.java
@@ -1,68 +1,108 @@
 package lab02;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Hashtable;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-
+		
+		System.out.flush();
 		Scanner s = new Scanner(System.in);
 		String arg;
-
-		List<String> classStrings = new ArrayList<String>();
-		List<Class> classArray = new ArrayList<Class>();
 		
+		Object selectedObject = null;
+		Hashtable<String, Object> table = new Hashtable<String, Object>();
+		//Object results = null;
+		Object results;
 		while (true) {
-
 			arg = s.nextLine();
 			String[] tokens = arg.split("[ ]+");
-
+			ArrayList<String> arguments = new ArrayList<String>();
+			for(int i = 1;i<tokens.length;i++){
+				arguments.add(tokens[i]);
+			}
 			switch (tokens[0]) {
-
 			case "Class":
-
 				Class<?> c;
 				try {
 					c = Class.forName(tokens[1]);
-					classArray.add(c);
-					classStrings.add(tokens[1]);
+					table.put(tokens[1], c);
+					selectedObject = c;
 					System.out.println(c.toString());
-
 				} catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
 				break;
-				
 			case "Set":
-				classStrings.set(classStrings.size() - 1, tokens[1]);
+				table.put(tokens[1], selectedObject);
+				System.out.println("Saved name for object of type: " + selectedObject.getClass().toString());
+				System.out.println(selectedObject.toString());
 				break;
-				
 			case "Get":
-				for (int i = classArray.size() - 1; i >= 0; i--) {
-
-					if (classStrings.get(i).equals(tokens[1])) {
-						System.out.println(classArray.get(i).toString());
-						break;
-					}
-				}
+				Object o = table.get(tokens[1]);
+				System.out.println(o.toString());
 				break;
-				
 			case "Index":
-				System.out.println(classArray.get(Integer.parseInt(tokens[1])).toString());
-				
+					Object[] array = (Object[])selectedObject;
+					selectedObject = array[Integer.parseInt(tokens[1])];
+					String print = selectedObject.toString();
+					System.out.println(print);
 				break;
-				
 			default:
-				System.out.println("Wrong Command");
+				try {
+					System.out.println("Trying generic command: " + arg);
+					Class<?> input[] = new Class[arguments.size()];
+				    for (int i = 0; i < arguments.size(); i++) {
+				        input[i] = arguments.get(i).getClass();
+				    }
+					if(arguments.isEmpty()){
+						Method m = selectedObject.getClass().getMethod(tokens[0], null);
+						results = m.invoke(selectedObject, null);
+						selectedObject = results;
+					}
+					else {
+						Method m = selectedObject.getClass().getMethod(tokens[0], input);
+						results = m.invoke(selectedObject, arguments);
+						selectedObject = results;
+					}
+					for(Object o2 : (Object[])results)
+						System.out.println(o2.toString());
+				} catch (NoSuchMethodException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SecurityException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
 				break;
-
 			}
-
 		}
 	}
-}
+	
+	static Method bestMethod(Class<?> type, String name, Class<?> argType ) throws	NoSuchMethodException{
+		try {
+			System.out.println(type.toString());
+			return	type.getMethod(name, argType);
+		}
+		catch(NoSuchMethodException	e) {
+			if(argType == Object.class) {
+				throw	new	NoSuchMethodException(name);
+			}
+			else {
+				return	bestMethod(type, name, argType.getSuperclass());
+			}
+		}
+	}
