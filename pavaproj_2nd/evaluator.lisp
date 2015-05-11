77f2d334767f013fef165830c798f34338837c3c
Joao Miranda <joaoshmiranda@hotmail.com>
2015-05-11 19:45:02 +0100

    shape is now fixed

--- pavaproj_2nd/evaluator.lisp
+++ pavaproj_2nd/evaluator.lisp
@@ -32,10 +32,31 @@
                               0
                               1))
             tensor))
-                                        
-
+                         
+(defun shape (tensor)
+    (let ((res nil))
+        (invert-first-and-second (shape-aux tensor res))))
+        
+(defun invert-first-and-second (list-to-invert)
+    (if (< (length list-to-invert) 2)
+        list-to-invert
+        (append (list (second list-to-invert))
+                (list (first list-to-invert))
+                (if (> (length list-to-invert) 2) ;; Prevents accessing null cdr
+                    (cddr list-to-invert)))))
+            
+(defun shape-aux (tensor res-list)
+    (if (listp tensor)
+        (shape-aux (car tensor) (append (list (length tensor)) res-list))
+        res-list))
 
 #|
+
+(shape '((((1 2 3) (4 5 1))
+          ((1 2 3) (4 5 1)))
+         (((1 2 3) (4 5 1))
+          ((1 2 3) (4 5 1)))))
+
     
 (defun .* (tensor)
     (mapcar #'* tensor))
