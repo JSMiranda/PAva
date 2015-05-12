b3b8abc4dd5c924b4304180047b9aef717e0581c
rnjesuz <rn_jesuz@hotmail.com>
2015-05-12 23:45:08 +0100

    dyadic .+ done

--- pavaproj_2nd/evaluator.lisp
+++ pavaproj_2nd/evaluator.lisp
@@ -1,5 +1,5 @@
 (defclass tensor ()
-  ((lst 		:accessor tensor-lst
+  ((lst 		:accessor lst
 				:initform '(1 2 3)
 				:initarg :lst)
    (randomarg 	:accessor randomarg
@@ -9,7 +9,6 @@
 (defun make-tensor (lst)
   (make-instance 'tensor :lst lst))	
 
-  
 (defun apl (expr)
     (eval expr))
 
@@ -18,32 +17,34 @@
 
 (defun v (&rest args)
     (make-tensor args))
+	
+	;;;;;;MONADIC;;;;;;
     
-(defun .- (tensor)
-    (mapcar #'- tensor))
+(defun .- ((t1 tensor))
+    (mapcar #'- (lst t1)))
     
-(defun ./ (tensor)
-    (mapcar #'/ tensor))
+(defun ./ ((t1 tensor))
+    (mapcar #'/ (lst t1)))
     
-(defun .! (tensor)
-    (mapcar #'fact tensor))
+(defun .! ((t1 tensor))
+    (mapcar #'fact (lst t1)))
     
 (defun fact (n)
   (if (< n 2)
       1
     (* n (fact(- n 1)))))
     
-(defun .sin (tensor)
-    (mapcar #'sin tensor))
+(defun .sin ((t1 tensor))
+    (mapcar #'sin (lst t1)))
     
-(defun .cos (tensor)
-    (mapcar #'cos tensor))
+(defun .cos ((t1 tensor))
+    (mapcar #'cos (lst t1)))
     
-(defun .not (tensor)
+(defun .not ((t1 tensor))
     (mapcar (lambda (val) (if (zerop val)
                               0
                               1))
-            tensor))
+            (lst t1)))
                          
 (defun shape (tensor)
     (let ((res nil))
@@ -65,8 +66,23 @@
 (defun interval (number)
 	(let ((res nil))
         (dotimes (count number)
-			(setf res (append res (list count))))
+			(setf res (append res (list (+ 1 count)))))
 		(make-tensor res)))
+
+;;;;;;DYADIC;;;;;;;
+(defun .+ ((tensor1 tensor) (tensor2 tensor))
+	(let ((res nil))
+	(cond 	((= (length (lst tensor1))(length (lst tensor2)))
+				(dotimes (count (length (lst tensor1)))
+					(setf res (append res (list (+ (nth count (lst tensor1)) (nth count(lst tensor2))) )))))
+			((= 1 (length (lst tensor1)))
+				(dotimes (count (length (lst tensor2)))
+					(setf res (append res (list (+ (nth 0 (lst tensor1)) (nth count (lst tensor2))))))))
+			((= 1 (length (lst tensor2)))
+				(dotimes (count (length (lst tensor1)))
+					(setf res (append res (list (+ (nth 0 (lst tensor2)) (nth count (lst tensor1))))))))
+			(t (princ "tensors not of the same size")))
+	(make-tensor res)))
 
 #|
 
